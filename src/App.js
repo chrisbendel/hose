694cd51088512bf6150a280235819472daf08828
Chris Bendel <chrissbendel@gmail.coml>
2017-12-03 15:04:36 -0500

    reroute plz work

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default class App extends Component {
                 <Route path="/shows/:type?/:id?" component={Shows}/>
                 <Route path="/song/:id?" component={Songs}/>
             </main>
-            <Redirect from="/" exact to="/shows"/>
+            <Redirect from="/" to="/shows"/>
           </div>
         </Router>
         <footer className="footer">
