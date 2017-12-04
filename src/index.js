60ef091dc28fff34033c5007cc3a7ab79c8de5a1
Chris Bendel <chrissbendel@gmail.coml>
2017-12-04 16:11:39 -0500

    add nucleus, remove unused deps

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const Nucleus = window.require('electron-nucleus');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+Nucleus.init('5a25ab8246d5e3641a3ef40a', true);
