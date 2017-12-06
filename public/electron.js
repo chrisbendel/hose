c81496f754ab6ccbf8180bfcfe359720dbf4487a
Chris Bendel <chrissbendel@gmail.com>
2017-12-06 14:21:51 -0500

    stuff

--- public/electron.js
+++ public/electron.js
@@ -8,6 +8,29 @@ let mainWindow;
 const isElectron = require('is-electron');
 const isDev = require('electron-is-dev');
 const autoUpdater = require("electron-updater").autoUpdater;
+autoUpdater.checkForUpdatesAndNotify();
+autoUpdater.addListener('update-available', () => {
+  dialog.showMessageBox({
+    type:      'info',
+    title:     'Hose Update',
+    message:   'A new version of ' + app.getName() + ' is ready to install!',
+    buttons:   ['Download the update now!', 'Not now'],
+    defaultId: 0,
+    cancelId:  1,
+  }, (buttonIndex) => {
+    if (buttonIndex === 0) {
+      setImmediate(() => {
+        app.removeAllListeners("window-all-closed")
+        willQuitApp = true;
+        if (mainWindow != null) {
+          mainWindow.close()
+        }
+        autoUpdater.quitAndInstall(false)
+        app.quit();
+      })
+    }
+  });
+});
 
 if (isElectron()) {
   prefs = {
@@ -51,25 +74,8 @@ function createWindow() {
 }
 
 app.on('ready', () => { 
-  autoUpdater.checkForUpdatesAndNotify();
 
   createWindow();
-    autoUpdater.addListener('update-available', () => {
-      dialog.showMessageBox({
-        type:      'info',
-        title:     'Hose Update',
-        message:   'A new version of ' + app.getName() + ' is ready to install!',
-        buttons:   ['Download the update now!', 'Not now'],
-        defaultId: 0,
-        cancelId:  1,
-      }, (buttonIndex) => {
-
-        if (buttonIndex === 0) {
-          shell.openExternal('https://chrissbendel.github.io/hose/');
-        }
-      });
-    })
-
 
 });
 
