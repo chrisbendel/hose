e451fda749d54111bfa122f24ad46823b803ec0b
Chris Bendel <chrissbendel@gmail.coml>
2017-12-06 11:29:14 -0500

    updates send you to site instead

--- public/electron.js
+++ public/electron.js
@@ -3,12 +3,11 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
-const {ipcMain} = require('electron');
+const {ipcMain, dialog, shell} = require('electron');
 let mainWindow; 
 const isElectron = require('is-electron');
 const isDev = require('electron-is-dev');
 const autoUpdater = require("electron-updater").autoUpdater;
-autoUpdater.checkForUpdatesAndNotify();
 
 if (isElectron()) {
   prefs = {
@@ -51,8 +50,27 @@ function createWindow() {
   }
 }
 
-app.on('ready', () => {
+app.on('ready', () => { 
+  autoUpdater.checkForUpdatesAndNotify();
+
   createWindow();
+    autoUpdater.addListener('update-available', () => {
+      dialog.showMessageBox({
+        type:      'info',
+        title:     'Hose Update',
+        message:   'A new version of ' + app.getName() + ' is ready to install!',
+        buttons:   ['Download the update now!', 'Not now'],
+        defaultId: 0,
+        cancelId:  1,
+      }, (buttonIndex) => {
+
+        if (buttonIndex === 0) {
+          shell.openExternal('https://chrissbendel.github.io/hose/');
+        }
+      });
+    })
+
+
 });
 
 app.on('window-all-closed', function () {
