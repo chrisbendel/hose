130973801dc97af04f9f77bb40ec3b0f5a2104f3
Chris Bendel <chrissbendel@gmail.com>
2018-02-02 10:04:14 -0500

    radio shell

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Controls from './../Controls';
 import {emitter} from './../Emitter';
 import Ionicon from 'react-ionicons';
-import {fetchRandomTrack} from './../api/phishin';
+import {history} from './../History';
+import {fetchRandomTrack, show} from './../api/phishin';
 import './../css/Radio.css';
 
 export default class Radio extends Component {
@@ -10,13 +11,14 @@ export default class Radio extends Component {
     super(props);
     this.state = {
       currentTrack: null,
+      currentShow: null
     };
   }
 
   componentWillMount() {
     emitter.addListener('songUpdate', (show, track, position, playing) => {
       this.setState({
-        playingShow: show,
+        currentShow: show,
         currentTrack: track,
         playingPosition: position,
         playing: playing
@@ -26,12 +28,18 @@ export default class Radio extends Component {
 
   fetchTrack() {
     fetchRandomTrack().then(track => {
-      this.setState({currentTrack: track})
+      show(track.show_id).then(show => {
+        this.setState({
+          currentTrack: track,
+          currentShow: show
+        });
+      });
     });
   }
 
   render() {
     let track = this.state.currentTrack;
+    let show = this.state.currentShow;
 
     if (!track) {
       return (
@@ -42,24 +50,33 @@ export default class Radio extends Component {
         </div>
       )
     }
-
+    console.log(track)
+    console.log(show)
     return (
       <div className="radio-container"> 
         <div className="information">
-          <div className="show-overview">
-            <img 
-              className="art"
-              alt={track.show_date} src={'https://s3.amazonaws.com/hose/images/' + track.show_date + '.jpg'}
-            />
-          </div>
-          <div> show info on the right </div>
-          <div> blahblah </div>
+          <img 
+            className="art"
+            alt={track.show_date} src={'https://s3.amazonaws.com/hose/images/' + track.show_date + '.jpg'}
+            onClick={() => {
+              history.push("/show/" + show.id)
+            }}
+          />
+          <div className="show-details">
+            <div>{show.date}</div>
+            <div>{show.venue.name}</div>
+            <div>{show.venue.location}</div>
+          </div>    
         </div>
-
+          
+        <div className="track-info">
+          <div> text </div>
+          <div> text2 </div>
+        </div>
         <div className="player">
-          <Ionicon className="control" icon="ios-skip-backward" fontSize="120px" />
-          <Ionicon className="control" icon="ios-play" fontSize="120px" />
-          <Ionicon className="control" icon="ios-skip-forward" fontSize="120px" />
+          <Ionicon className="control" icon="ios-skip-backward" fontSize="50px" />
+          <Ionicon className="control" icon="ios-play" fontSize="50px" />
+          <Ionicon className="control" icon="ios-skip-forward" fontSize="50px" />
         </div>
       </div>
     );
