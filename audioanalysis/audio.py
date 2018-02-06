9a36cb45460d3100cc5d2a1906eb7c2d90643f50
Chris Bendel <chrissbendel@gmail.coml>
2018-02-06 10:29:49 -0500

    clusters

--- audioanalysis/audio.py
+++ audioanalysis/audio.py
@@ -22,9 +22,20 @@ import matplotlib.pyplot as plt
 
 path = os.getcwd() + "/songs/"
 
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep].astype(np.float64)
+
 def deleteMP3s():
     for filename in os.listdir(path):
         if filename.endswith(".mp3"):
+            os.remove(path + filename)
+
+def deleteWAVs():
+    for filename in os.listdir(path):
+        if filename.endswith(".wav"):
             os.remove(path + filename)
 
 def downloadMP3s(url):
@@ -45,38 +56,34 @@ def extractAudioFeatures():
         trackID = str(filename[i].split('/')[-1]).replace(".wav", "")
         d[trackID] = features[i].tolist()
 
-    with open("output.csv", "wb") as f_output:
+    with open("output.csv", "a") as f_output:
         csv_output = csv.writer(f_output)
 
         for key in sorted(d.keys()):
             csv_output.writerow([key] + d[key])
 
-# start_time = time.time()
+    deleteWAVs()
 
-# res = requests.get("http://phish.in/api/v1/tracks?tag=Jamcharts&per_page=200&sort_attr=likes_count&sort_dir=desc").json()
-# urls = getMP3Urls(res['data'])
-# res = requests.get("http://phish.in/api/v1/songs/black-eyed-katy").json()
-# urls = getMP3Urls(res['data']['tracks'])
-# res = requests.get("http://phish.in/api/v1/songs/nellie-kane").json()
-# urls = urls + getMP3Urls(res['data']['tracks'])
-# res = requests.get("http://phish.in/api/v1/songs/the-moma-dance").json()
-# urls = getMP3Urls(res['data']['tracks'])
-# pool = Pool()
-# pool.map(downloadMP3s, urls)
-
-# convertDirMP3ToWav(path, 16000, 1, False)
-# deleteMP3s()
-# extractAudioFeatures()
-
+#start_time = time.time()
 # Pagination stuff for doing all songs
-# num_pages = res['total_pages']
-# for page in range(1, num_pages + 1):
-#     res = requests.get("http://phish.in/api/v1/tracks?per_page=30", params={'page': page}).json()
-#     print res['page']
+#res = requests.get("http://phish.in/api/v1/tracks").json()
+#num_pages = res['total_pages']
+#for page in range(1, num_pages + 1):
+#    print(page)
+#    res = requests.get("http://phish.in/api/v1/tracks", params={'page': page}).json()
+#    urls = getMP3Urls(res['data'])
+#    pool = Pool()
+#    pool.map(downloadMP3s, urls)
+#    convertDirMP3ToWav(path, 16000, 1, False)
+#    deleteMP3s()
+#    extractAudioFeatures()
+#    pool.close()
 
-# print("--- %s seconds ---" % (time.time() - start_time))
+#print("--- %s seconds ---" % (time.time() - start_time))
 
-songs = pd.read_csv("output.csv", header=None)
+#songs = pd.read_csv("output.csv", header=None).fillna(0)
+songs = pd.read_csv("output.csv", header=None, nrows=20).fillna(0)
+copy = songs
 numClusters = 15
 
 # ==== This section is for setting our initial centroids, if we choose to === #
@@ -104,21 +111,24 @@ numClusters = 15
 # remove the ids for clustering
 data = songs.drop(songs.columns[0], axis=1)
 data = (data - data.mean()) / (data.max() - data.min())
+data = clean_dataset(data)
 km = KMeans(n_clusters=numClusters, init='k-means++', n_jobs=-1, n_init=50)
 km.fit(data)
-x = km.fit_predict(data)
-songs['Cluster'] = x
 
+x = km.fit_predict(data)
+
+songs['Cluster'] = x
 with open('songs.csv', 'wb') as outcsv:
     writer = csv.writer(outcsv)
-    writer.writerow(["Song", "Date", "Set", "Cluster"])
-    
+    writer.writerow(["ID", "Song", "Date", "Set", "Cluster"])
+  
     for i in range(numClusters):
-        # print ("Cluster: " + str(i))
+        print ("Cluster: " + str(i))
         for x in songs[songs['Cluster'] == i][0]:
             res = requests.get("http://phish.in/api/v1/tracks/" + str(x)).json()
-            writer.writerow([res['data']['title']] + [res['data']['show_date']] + [res['data']['set']] + [i])
-            # print(res['data']['title'], res['data']['show_date'], res['data']['id'])
-        # print("<-------------->")
+            writer.writerow([res['data']['id']] + [res['data']['title']] + [res['data']['show_date']] + [res['data']['set']] + [i])
+            print(res['data']['title'], res['data']['show_date'], res['data']['id'])
+        print("<-------------->")
 
-# songs.to_csv("clusters.csv", index = False)
+songs.to_csv("clusters.csv", index = False)
+
