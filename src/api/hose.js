69fb2f8b9891dfc7475b4473d64f67ac6ea7eb37
jonahallibone <jonah.allibone@mymail.champlain.edu>
2018-03-14 20:13:10 -0400

    added links

---
+++ src/api/hose.js
@@ -0,0 +1,6 @@
+[
+  "https://hose-api.herokuapp.com/api/song/listen",
+  "https://hose-api.herokuapp.com/api/song/like",
+  "https://hose-api.herokuapp.com/api/song/dislike",
+  "https://hose-api.herokuapp.com/api/song/completed"
+]
