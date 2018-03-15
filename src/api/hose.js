6b541bb579c54825fabbb5817f20b34ce9965191
Chris Bendel <chrissbendel@gmail.coml>
2018-03-14 20:22:29 -0400

    test

--- src/api/hose.js
+++ src/api/hose.js
@@ -1,6 +1,20 @@
 [
-  "https://hose-api.herokuapp.com/api/song/listen",
-  "https://hose-api.herokuapp.com/api/song/like",
-  "https://hose-api.herokuapp.com/api/song/dislike",
-  "https://hose-api.herokuapp.com/api/song/completed"
+  "https://hose-api-dev.herokuapp.com/api/song/listen",
+  "https://hose-api-dev.herokuapp.com/api/song/like",
+  "https://hose-api-dev.herokuapp.com/api/song/dislike",
+  "https://hose-api-dev.herokuapp.com/api/song/completed"
 ]
+const base = "https://hose-api-dev.herokuapp.com/api/";
+
+export const likeTrack = id => {
+  fetch(base + "song/like", {
+    method: "POST",
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      song_id: id
+    })
+  })
+}
